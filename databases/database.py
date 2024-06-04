0e299e835a7b9c322b9badf2ce6ca879d582065c
Chetan <sonigrachetan44@gmail.com>
2024-06-05 02:40:41 +0530

    BugFix: Use of sessionmaker instead of Session

--- databases/database.py
+++ databases/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm.session import Session
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./blog_api.db"
@@ -7,12 +7,12 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./blog_api.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread": False})
 
-session_local = Session(bind=engine,autocommit=False,autoflush=False)
+SessionLocal = sessionmaker(bind=engine,autocommit=False,autoflush=False)
 
 Base = declarative_base()
 
 def get_db():
-    db = session_local()
+    db = SessionLocal()
     try:
         yield db
     finally:
