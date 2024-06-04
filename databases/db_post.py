2d5c04fa90d6072858e4c2bdb3d37dba5867fe9b
Chetan <sonigrachetan44@gmail.com>
2024-06-05 03:19:38 +0530

    Adds functionality of getting and deleting posts and endpoints

--- databases/db_post.py
+++ databases/db_post.py
@@ -2,6 +2,7 @@ from routers.schemas import PostBase
 from .models import DbPost
 from sqlalchemy.orm.session import Session
 from datetime import datetime
+from fastapi import HTTPException,status
 
 
 def create_post(request: PostBase,db: Session) -> DbPost:
@@ -15,3 +16,15 @@ def create_post(request: PostBase,db: Session) -> DbPost:
     db.commit()
     db.refresh(new_post)
     return new_post
+
+def get_all(db: Session):
+    return db.query(DbPost).all()
+
+def delete_post(db: Session, id: int):
+    post = db.query(DbPost).filter(DbPost.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} not found.")
+    db.delete(post)
+    db.commit()
+    return f"Post with id {id} deleted."
--- routers/post.py
+++ routers/post.py
@@ -1,14 +1,24 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends, UploadFile, File
 from databases.database import get_db
 from databases import db_post
 from sqlalchemy.orm.session import Session
 from routers.schemas import PostBase,PostDisplay
+import string
 
 router = APIRouter(prefix="/post",
                    tags=["post"]
                 )
 
-@router.post("")
+@router.post("",response_model=PostDisplay)
 def create_post(request:PostBase,db: Session = Depends(get_db)):
     return db_post.create_post(request,db)
     
+
+@router.get("/all",response_model=list[PostDisplay])
+def get_posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
+
+
+@router.delete("/delete")
+def delete_post(id: int, db: Session = Depends(get_db)):
+    return db_post.delete_post(db,id)
