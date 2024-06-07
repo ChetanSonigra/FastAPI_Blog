6d721cb7d9913e652595096ed40aadc0a5497869
Chetan <sonigrachetan44@gmail.com>
2024-06-07 16:33:03 +0530

    changes origins to use environment variable

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from databases import models
 from databases.database import engine
 from routers import post
+import os
 
 app = FastAPI()
 app.include_router(post.router)
@@ -13,7 +14,7 @@ models.Base.metadata.create_all(engine)
 app.mount(path="/images",app=staticfiles.StaticFiles(directory="images"),name="images")
 
 origins = [
-    "http://localhost:3000"
+    os.environ.get("origin")
 ]
 
 app.add_middleware(CORSMiddleware,
